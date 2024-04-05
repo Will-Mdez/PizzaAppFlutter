4bda40ed1164e23b484b5f48fae7811caf970276
William Mendez <mate282010@hotmail.com>
2024-04-05 13:46:23 -0600

    Pizza App Flutter lib set

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:myappflutter/app.dart';
+import 'package:myappflutter/simple_bloc_observer.dart';
+import 'package:user_repository/user_repository.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  Bloc.observer = SimpleBlocObserver();
+  runApp(MyApp(FirebaseUserRepo()));
+}
